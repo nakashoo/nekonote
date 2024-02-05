f62f731025bd4de06357d6fd9e6c2729fdba3833
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2024-02-05 17:34:02 +0000

    <add>カスタマーとアドミンのDB作成

--- db/migrate/20240205160030_devise_create_admins.rb
+++ db/migrate/20240205160030_devise_create_admins.rb
@@ -7,6 +7,9 @@ class DeviseCreateAdmins < ActiveRecord::Migration[6.1]
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
+      t.string :name,       null: false
+      t.string :responsible,null: false
+
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
