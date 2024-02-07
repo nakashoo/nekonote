1faa787efd2963d8084e9e61690d05377359aad3
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2024-02-07 01:56:23 +0000

    <add>admin、customerのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,34 @@
 Rails.application.routes.draw do
 
+  # 運営用
+  # URL /admin/sign_in ...
+  devise_for :admin, skip: [:passwords] ,controllers: {
+    registrations: "admin/registrations",
+    sessions: "admin/sessions"
+  }
 
-  devise_for :admins
-  devise_for :customers
+  # サークル員用
+  # URL /customers/sign_in ...
+  devise_for :customers,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+ # 運営用
+  namespace :admin do
+   get 'admins/my_page' => 'admins#show'
+   get 'admins/infomation/edit' => 'admins#edit'
+   patch 'admins/information' => 'admins#update'
+   post 'admins/my_page' => 'admins#show', as: 'admin_admins_my_page'
+  end
+
+
+ # サークル用
+  scope module: :public do
+    get 'customers/my_page' => 'customers#show'
+    get 'costomers/infomation/edit' => 'customers#edit'
+    patch 'customers/information' => 'customers#update'
+  end
+
+
 end
