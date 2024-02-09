46a80e3ba95f3d5f2ee7dd4754e75bcf79ff173a
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2024-02-09 20:17:12 +0000

    <add>adminのnoteのshowページ搭載

--- app/controllers/admin/notes_controller.rb
+++ app/controllers/admin/notes_controller.rb
@@ -1,13 +1,34 @@
 class Admin::NotesController < ApplicationController
   def index
+    @notes = Note.page(params[:page])
   end
 
   def new
+    @note = Note.new
+  end
+
+  def create
+   @note = Note.new(note_params)
+   @note.admin = current_admin
+   if @note.save
+    redirect_to admin_note_path(@note), notice: "You have created book successfully."
+   else
+    index
+    render 'index'
+   end
   end
 
   def show
+    @note = Note.find(params[:id])
   end
 
   def edit
   end
+
+  private
+
+  def note_params
+    params.require(:note).permit(:title,:body)
+  end
+
 end
