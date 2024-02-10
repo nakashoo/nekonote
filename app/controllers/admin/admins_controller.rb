3eec8edba27e9e561a0ce2a346564a2f9661d1f6
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2024-02-10 12:10:48 +0000

    <add>admin　mypageのshow、edit、update機能搭載

--- app/controllers/admin/admins_controller.rb
+++ app/controllers/admin/admins_controller.rb
@@ -1,7 +1,28 @@
 class Admin::AdminsController < ApplicationController
+
   def show
+    @current_admin = current_admin
+    render :show
   end
 
   def edit
+    @current_admin = current_admin
   end
+
+  def update
+     @customer = current_customer
+    if @customer.update(customer_params)
+      redirect_to admin_admins_my_page _path
+    else
+      render "edit"
+    end
+  end
+
+    private
+
+  def customer_params
+    params.require(:admin).permit(:name,:responsible,:image)
+  end
+
+
 end
