11f6bc89c2f1767c7af9eb1297f136b03ecac5c7
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2024-02-12 00:39:43 +0000

    <finish>customerのサイドバーの編集、更新完成

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,4 +4,20 @@ class Public::CustomersController < ApplicationController
 
   def edit
   end
+
+  def update
+    @current_customer = current_customer
+   if @current_customer.update(current_customer_params)
+     redirect_to customers_my_page_path
+   else
+     render "edit"
+   end
+  end
+
+    private
+
+  def current_customer_params
+    params.require(:customer).permit(:name,:member_ship_year,:image)
+  end
+
 end
